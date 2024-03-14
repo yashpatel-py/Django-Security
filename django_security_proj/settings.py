5bc7d57bcdee5be7092659cefd395ff2f5589b31
yashpatel-py <mailforyash04@gmail.com>
2024-03-14 11:41:29 -0500

    STATIC and MEDIA SETUP

--- django_security_proj/settings.py
+++ django_security_proj/settings.py
@@ -116,7 +116,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
+MEDIA_URL = '/images/'
+MEDIA_ROOT = BASE_DIR / "static/media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
